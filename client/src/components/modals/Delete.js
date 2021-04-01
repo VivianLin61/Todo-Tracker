42a62fbd289d4d5a8f4ecf5cf33a477b9f64d365
Vivian Lin <linvivian61@gmail.com>
2021-03-31 21:12:27 -0400

    fix modal layout

--- client/src/components/modals/Delete.js
+++ client/src/components/modals/Delete.js
@@ -1,33 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-import { WModal, WMHeader, WMMain, WButton } from 'wt-frontend';
+import { WModal, WMHeader, WMMain, WButton, WMFooter } from 'wt-frontend'
 
 const Delete = (props) => {
+  const handleDelete = async () => {
+    props.deleteList(props.activeid)
+    props.setShowDelete(false)
+  }
 
-    const handleDelete = async () => {
-        props.deleteList(props.activeid);
-        props.setShowDelete(false);
-    }
-
-    return (
-        // Replace div with WModal
-        <div className="delete-modal">
-            <div className="modal-header" onClose={() => props.setShowDelete(false)}>
-                Delete List?
-			</div>
-
-            <div>
-                <WButton className="modal-button cancel-button" onClick={() => props.setShowDelete(false)} wType="texted">
-                    Cancel
-				</WButton>
-                <label className="col-spacer">&nbsp;</label>
-                <WButton className="modal-button" onClick={handleDelete} clickAnimation="ripple-light" hoverAnimation="darken" shape="rounded" color="danger">
-                    Delete
-				</WButton>
-            </div>
-
-        </div>
-    );
+  return (
+    <div>
+      <WModal
+        className='delete-modal'
+        visible={props.setShowDelete}
+        cover={true}
+        animation='slide-fade-top'
+      >
+        <WMHeader
+          className='modal-header'
+          onClose={() => props.setShowDelete(false)}
+        >
+          Delete List?
+        </WMHeader>
+        <WMFooter float='center'>
+          <WButton
+            className='modal-button cancel-button'
+            onClick={() => props.setShowDelete(false)}
+            wType='texted'
+          >
+            Cancel
+          </WButton>
+          <label className='col-spacer'>&nbsp;</label>
+          <WButton
+            className='modal-button'
+            onClick={handleDelete}
+            clickAnimation='ripple-light'
+            hoverAnimation='darken'
+            shape='rounded'
+            color='danger'
+          >
+            Delete
+          </WButton>
+        </WMFooter>
+      </WModal>
+    </div>
+  )
 }
 
-export default Delete;
+export default Delete
